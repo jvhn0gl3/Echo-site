71b7d1a2f6d21e22c624c4146358f619abbd6509
john-ogletree <pxltr3@gmail.com>
2026-04-22 00:08:15 -0400

    chore: remove redundant modular CSS and JS files after master consolidation

--- js/forms.js
+++
@@ -1,6 +0,0 @@
-// Form Handlers
-document.getElementById('contactForm')?.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('[SUCCESS] Transmission received. Processing response...');
-    e.target.reset();
-});
