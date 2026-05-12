25ab31056e2b2eb89b444e98f8badbf825bbd031
John Ogletree <jvhn0gl3@proton.me>
2026-05-12 02:40:35 -0400

    deprecate ES6 by inlining directly

--- js/master.js
+++
@@ -1,9 +0,0 @@
-// master.js - Main JavaScript module that imports all other JS modules
-
-// Import sidebar component
-import './sidebar.js';
-
-// Import notifications module
-import './notifications.js';
-
-console.log('Master JS module loaded - Echo Portfolio');
